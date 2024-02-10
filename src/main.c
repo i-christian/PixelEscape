8614f02f1148975ca0c17aded5f49def20c12fa6
Christian <111076892+i-christian@users.noreply.github.com>
2024-02-11 01:58:07 +0200

    update main.c

--- src/main.c
+++ src/main.c
@@ -2,20 +2,6 @@
 #include <SDL2/SDL_audio.h>
 #include <stdio.h>
 
-/*const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
-	{R, R, R, R, R, R, R, R, R, R},
-	{R, 0, 0, 0, 0, 0, 0, 0, 0, R},
-	{R, 0, 0, G, G, G, G, G, 0, R},
-	{R, 0, 0, G, 0, 0, 0, G, 0, R},
-	{R, 0, 0, G, 0, G, 0, G, 0, R},
-	{R, 0, 0, 0, 0, G, 0, G, 0, R},
-	{R, 0, G, G, G, G, 0, G, 0, R},
-	{R, 0, 0, 0,
-		0, 0, 0, G, 0, R},
-	{R, 0, 0, 0, 0, 0, 0, 0, 0, R},
-	{R, R, R, R, R, R, R, R, R, R}
-};*/
-
 const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] =
 {
   {R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R},
@@ -26,7 +12,7 @@ const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] =
   {R,0,0,0,0,0,G,0,0,0,G,0,0,0,0,0,0,0,0,0,0,0,0,R},
   {R,0,0,0,0,0,G,0,0,0,G,0,0,0,0,B,0,0,0,B,0,0,0,R},
   {R,0,0,0,0,0,G,0,0,0,G,0,0,0,0,0,0,0,0,0,0,0,0,R},
-  {R,0,0,0,0,0,G,G,0,G,G,0,0,0,0,B,0,B,0,B,0,0,0,R},
+  {R,0,0,0,0,0,G,G,0,G,G,0,0,0,0,B,B,B,0,B,0,0,0,R},
   {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
   {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
   {R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,R},
@@ -47,30 +33,39 @@ const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] =
 
 /**
  * render - rendering function
+ * @gameData: Pointer to game data struct
+ *
  * Return: Nothing
  */
 void render(gameData *gameData)
 {
-	if (gameData->showMap)
-	{
-		clearRenderer();
-		renderOverheadMap(gameData);
-	} else
-	{
-		/* Draw projected scene */
-		renderProjectedScene(gameData);
-	}
+    if (gameData->showMap)
+    {
+        clearRenderer();
+        renderOverheadMap(gameData);
+    }
+    else
+    {
+        /* Draw projected scene */
+        renderProjectedScene(gameData);
+    }
 }
 
-
-int setupWindow(gameData *gameData) {
+/**
+ * setupWindow - Sets up the game window and initializes textures
+ * @gameData: Pointer to game data struct
+ *
+ * Return: TRUE if setup was successful, FALSE otherwise
+ */
+int setupWindow(gameData *gameData)
+{
     int x, y;
 
-    if(!initGFX("A 3D Raycasting Maze Adventure",
-				WINDOW_WIDTH, WINDOW_HEIGHT))
-	{
-		return (FALSE);
-	}
+    if (!initGFX("A 3D Raycasting Maze Adventure",
+                 WINDOW_WIDTH, WINDOW_HEIGHT))
+    {
+        return (FALSE);
+    }
 
     gameData->screenBuffer = createTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
     gameData->redXorTexture = generateRedXorTexture(TEXTURE_SIZE);
@@ -82,72 +77,75 @@ int setupWindow(gameData *gameData) {
     gameData->TEXTURES[2] = gameData->blueXorTexture;
     gameData->TEXTURES[3] = gameData->grayXorTexture;
 
-    if(!gameData->screenBuffer)
-		return (FALSE);
+    if (!gameData->screenBuffer)
+        return (FALSE);
 
     /* Make the texture initially gray */
-    for(x = 0; x < WINDOW_WIDTH; x++)
-        for(y = 0; y < WINDOW_HEIGHT; y++)
+    for (x = 0; x < WINDOW_WIDTH; x++)
+        for (y = 0; y < WINDOW_HEIGHT; y++)
             gameData->screenBuffer[(WINDOW_WIDTH * y) + x] = 0xFFAAAAAA;
 
     return (TRUE);
 }
 
+/**
+ * initAudio - Initializes audio data and loads background music
+ * @audioData: Pointer to audio data struct
+ *
+ * Return: None
+ */
 void initAudio(AudioData *audioData)
 {
-	audioData->audioBuffer = NULL;
+    audioData->audioBuffer = NULL;
     audioData->audioLength = 0;
     audioData->rw = NULL;
 
-	if (!initSDLAudio(audioData))
-	{
-		fprintf(stderr, "failed to initialize audio");
+    if (!initSDLAudio(audioData))
+    {
+        fprintf(stderr, "failed to initialize audio");
+    }
 
-	}
-
-	if (!loadBackgroundMusic(audioData))
-	{
-		fprintf(stderr, "Failure to load Music\n");
-	}
+    if (!loadBackgroundMusic(audioData))
+    {
+        fprintf(stderr, "Failure to load Music\n");
+    }
 }
 
 /**
  * main - the main entry of the program
  * Return: exit status
  */
-
 int main(void)
 {
-
-	gameData gameData;
-	gameData.screenBuffer = NULL;
-	gameData.redXorTexture = NULL;
-	gameData.greenXorTexture = NULL;
-	gameData.blueXorTexture = NULL;
-	gameData.grayXorTexture = NULL;
-	/*initialize colors */
-	gameData.COLORS[0] = RGBtoABGR(255, 0, 0);
+    gameData gameData;
+    gameData.screenBuffer = NULL;
+    gameData.redXorTexture = NULL;
+    gameData.greenXorTexture = NULL;
+    gameData.blueXorTexture = NULL;
+    gameData.grayXorTexture = NULL;
+    /*initialize colors */
+    gameData.COLORS[0] = RGBtoABGR(255, 0, 0);
     gameData.COLORS[R] = RGBtoABGR(0, 255, 0);
     gameData.COLORS[2] = RGBtoABGR(0, 0, 255);
     gameData.COLORS[3] = RGBtoABGR(128, 128, 128);
-	/*program toggles */
-	gameData.gameIsRunning = TRUE;
-	gameData.showMap = TRUE;
-	gameData.distortion = FALSE;
-	gameData.slowRenderMode = 0;
-	gameData.rayCastMode = 0;
-	gameData.textureMode = 0;
+    /*program toggles */
+    gameData.gameIsRunning = TRUE;
+    gameData.showMap = TRUE;
+    gameData.distortion = FALSE;
+    gameData.slowRenderMode = 0;
+    gameData.rayCastMode = 0;
+    gameData.textureMode = 0;
 
-	if (!setupWindow(&gameData))
-	{
-		fprintf(stderr, "Could not initialize raycaster!\n");
-		return (EXIT_FAILURE);
-	}
+    if (!setupWindow(&gameData))
+    {
+        fprintf(stderr, "Could not initialize raycaster!\n");
+        return (EXIT_FAILURE);
+    }
 
-	initAudio(&audioData);
-	initPlayer();
-	initRaycaster();
-	runGame(&gameData, &audioData);
-	destroyGFX();
-	return (EXIT_SUCCESS);
+    initAudio(&audioData);
+    initPlayer();
+    initRaycaster();
+    runGame(&gameData, &audioData);
+    destroyGFX();
+    return (EXIT_SUCCESS);
 }
